adf46d2b3fe42080792f4e59ad50f8987c086828
thedarkknight13 <78083408+thedarkknight13@users.noreply.github.com>
2022-04-08 17:17:37 +0200

    Update app.py
    Added code to remove previously uploaded videos

--- app.py
+++ app.py
@@ -74,6 +74,17 @@ def search_for_obj():
                     shutil.rmtree(file_path)
             except Exception as e:
                 print('Failed to delete %s. Reason: %s' % (file_path, e))
+        # Then delete the videos currently in the uploads folder
+        folder = 'static/uploads'
+        for filename in os.listdir(folder):
+            file_path = os.path.join(folder, filename)
+            try:
+                if os.path.isfile(file_path) or os.path.islink(file_path):
+                    os.unlink(file_path)
+                elif os.path.isdir(file_path):
+                    shutil.rmtree(file_path)
+            except Exception as e:
+                print('Failed to delete %s. Reason: %s' % (file_path, e))
 
         
         filename = secure_filename(file.filename)
